f1ead83ad6e04d891345fa7e5ef4fe3c30f7e8d6
isauravmanitripathi <spmtripathi10@gmail.com>
2021-12-15 22:23:50 +0000

    pushing code

--- java-restful-web-services/usermanagement/src/user.java
+++ java-restful-web-services/usermanagement/src/user.java
@@ -1,2 +1,10 @@
-public class user {
+import java.io.Serializable;
+import javax.xml.*;
+
+public class user implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String name;
+    private String sector;
+    public user(){};
 }
